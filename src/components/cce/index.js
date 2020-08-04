1fb4c1f5598825444604910cd8c7788662ddad2d
M_Qadeer <30898601+MQadeer@users.noreply.github.com>
2020-08-04 12:51:55 +0500

    calculator update

--- src/components/cce/index.js
+++ src/components/cce/index.js
@@ -39,7 +39,7 @@ class NCCE extends Component {
         // console.log( parseFloat(e.target.value))
 
         var obj = {};
-        if (parseFloat(e.target.value) < 0 || e.target.value==NaN) {
+        if (parseFloat(e.target.value) < 0 || e.target.value == NaN) {
             e.target.value = 0;
             return
         }
@@ -52,24 +52,58 @@ class NCCE extends Component {
 
 
     calculateRoomCost = (D1, D2, nos) => {
-        console.log("nos ",nos)
-        if (nos == 0 || isNaN(nos)) {
-            return { bricks: 0, cement:0, sand:0 }
-        } else {
-            let d1 = 2 * (D1 * 12)
-            let d2 = 2 * (((D2 * 2) - 3) * 12)
-            const area = d1 + d2
-            console.log(d1, d2, area)
-            const totalBricks = (((area * 144) / 17.5) - 376) 
 
-            const volume = (area * 0.75) - ((121.5 * totalBricks) / 1728)
-            const wet = volume / 35.3147
-            const dry = wet * 1.33
-            const cement = (((1 / 7) * dry) * 28.8) * nos
-            const sand = (((6 / 7) * dry) * 35.314) * nos
+        let d1 = 2 * (D1 * 12)
+        let d2 = 2 * (((D2 * 2) - 3) * 12)
+        const area = d1 + d2
+        const totalBricks = (((area * 144) / 17.5) - 376)
 
-            console.log("room marterials ", totalBricks*nos, cement, sand);
-            return { bricks: (totalBricks* nos), cement, sand }
+        const volume = (area * 0.75) - ((121.5 * totalBricks) / 1728)
+        const wet = volume / 35.3147
+        const dry = wet * 1.33
+        const cement = (((1 / 7) * dry) * 28.8) * nos
+        const sand = (((6 / 7) * dry) * 35.314) * nos
+
+        console.log("room marterials ", totalBricks * nos, cement, sand);
+        return { bricks: (totalBricks * nos), cement, sand }
+
+    }
+
+    calculateRoomMaterials = (D1, D2, nos, type) => {
+        
+        const basicM = this.calculateRoomCost(D1, D2, nos);
+        const area = D1 + D2
+        if (type === "room") {
+            if (nos == 0 || isNaN(nos)) {
+                return { bricks: 0, cement: 0, sand: 0, paint: 0 }
+            } else {
+                const paint = (((D1 * 2) + (D2 * 2)) * 12) / 400  // 1 galon paint covers 400 sf
+                return { bricks: (basicM.bricks), cement: basicM.cement, sand: basicM.sand, paint: (paint * nos) }
+            }
+        }
+        else if (type === "bath") {
+            if (nos == 0 || isNaN(nos)) {
+                return { bricks: 0, cement: 0, sand: 0, paint: 0, tiles: 0 }
+            } else {
+                const paint = (((D1 * 2) + (D2 * 2)) * 3) / 400  // 1 galon paint covers 400 sf
+                const wallArea = (((D1 * 2) + (D2 * 2)) * 6) - 21
+                const tileArea = (30 * 30) * 0.001
+                const tiles = wallArea / tileArea
+                console.log("bath paint tiles", paint, tiles)
+                return { bricks: (basicM.bricks), cement: basicM.cement, sand: basicM.sand, paint: (paint * nos), tiles: (tiles * nos) }
+            }
+        }
+        else if (type === "kitchen") {
+            if (nos == 0 || isNaN(nos)) {
+                return { bricks: 0, cement: 0, sand: 0, paint: 0, tiles: 0 }
+            } else {
+                const paint = (((D1 * 2) + (D2 * 2)) * 9) / 400  // 1 galon paint covers 400 sf
+                const wallArea = (((D1 * 2) + (D2 * 2)) * 3) - 21
+                const tileArea = (30 * 30) * 0.001
+                const tiles = wallArea / tileArea
+                console.log("kitchen paint tiles", paint, tiles)
+                return { bricks: (basicM.bricks), cement: basicM.cement, sand: basicM.sand, paint: (paint * nos), tiles: (tiles * nos) }
+            }
         }
     }
 
@@ -78,23 +112,38 @@ class NCCE extends Component {
         let w2 = (d2 / 3.229) * ((d2 * 12) / 8) * 0.88
         const totalIron = (w1 + w2) * floors
 
+        const paint = ((d1 * d2) / 400) * floors // 1 galon paint covers 400 sf
         const wet = d1 * d2 * 0.416
         const dry = wet * 1.54
         const cement = (((1 / 7) * dry) / 1.25) * floors
         const sand = ((2 / 7) * dry) * floors
         const crush = ((4 / 7) * dry) * floors
         console.log("roof marterials ", totalIron, cement, sand, crush);
-        return { iron: totalIron, cement, sand, crush }
+        return { iron: totalIron, cement, sand, crush, paint }
     }
-    calculateFloorCost = (d1, d2) => {
+    calculateFloorCost = (d1, d2,nos) => {
 
+        const tileArea = (30 * 30) * 0.001
+        const tiles = (d1 * d2) / tileArea
         const wet = d1 * d2 * 0.16667
         const dry = wet * 1.54
         const cement = ((1 / 7) * dry) / 1.25
         const sand = (2 / 7) * dry
         const crush = (4 / 7) * dry
         console.log("floor marterials ", cement, sand, crush);
-        return { cement, sand, crush }
+        return { cement, sand, crush, tiles: (tiles * nos) }
+    }
+
+    otherCosts=()=>{
+        const plumbing=(16000*this.state.plotSize)*this.state.floor
+        const wireing=(13000*this.state.plotSize)*this.state.floor
+        const bathMaterial=(9000+15000+19000+50000)*this.state.baths //vanity set , commodes,bath set, bath hangings
+        const kithchenMaterial=(50000,15000)*this.state.kitchen //hood&knob , sink
+        const bkPlumbing=(this.state.baths+this.state.kitchen)*4000
+
+        const otherCosts=(plumbing+wireing+bathMaterial+kithchenMaterial+bkPlumbing)
+        return (otherCosts)
+
     }
     onSubmit = () => {
         if (this.state.plotSize == 0 || this.state.plotD1 == 0 || this.state.plotD2 == 0 || this.state.floor == 0) {
@@ -108,11 +157,11 @@ class NCCE extends Component {
         const kitchensqft = (this.state.kitchenD1 * this.state.kitchenD2) * this.state.kitchen
         const bathsqft = (this.state.bathD1 * this.state.bathD2) * this.state.baths
         const livingsqft = (this.state.livingD1 * this.state.livingD2) * this.state.livingRoom
-        const totalRoomsqft = roomsqft + kitchensqft + bathsqft+livingsqft
+        const totalRoomsqft = roomsqft + kitchensqft + bathsqft + livingsqft
 
         const marla = this.state.plotSize
         const sft = this.state.plotD1 * this.state.plotD2
-        const totalGivensqft = sft
+        const totalGivensqft = sft * this.state.floor
         console.log(totalGivensqft, totalRoomsqft)
 
         if (totalGivensqft < totalRoomsqft) {
@@ -124,27 +173,36 @@ class NCCE extends Component {
         }
 
         const roofMaterials = this.calculateRoofCost(this.state.plotD1, this.state.plotD2, this.state.floor)
-        const floorMaterials = this.calculateFloorCost(this.state.plotD1, this.state.plotD2)
-        const roomMaterials = this.calculateRoomCost(this.state.roomD1, this.state.roomD2, this.state.rooms)
-        const bathMaterials = this.calculateRoomCost(this.state.bathD1, this.state.bathD2, this.state.baths)
-        const kitchenMaterials = this.calculateRoomCost(this.state.kitchenD1, this.state.kitchenD2, this.state.kitchen)
-        const livingRoomMaterials = this.calculateRoomCost(this.state.livingD1, this.state.livingD2, this.state.livingRoom)
+        const floorMaterials = this.calculateFloorCost(this.state.plotD1, this.state.plotD2,this.state.floor)
+        const roomMaterials = this.calculateRoomMaterials(this.state.roomD1, this.state.roomD2, this.state.rooms, "room")
+        const bathMaterials = this.calculateRoomMaterials(this.state.bathD1, this.state.bathD2, this.state.baths, "bath")
+        const kitchenMaterials = this.calculateRoomMaterials(this.state.kitchenD1, this.state.kitchenD2, this.state.kitchen, "kitchen")
+        const livingRoomMaterials = this.calculateRoomMaterials(this.state.livingD1, this.state.livingD2, this.state.livingRoom, "room")
+        const otherCosts=this.otherCosts();
+        console.log("othercosts" , otherCosts)
 
         const totalMaterialsUsed = {
-            bricks: parseInt(roomMaterials.bricks + bathMaterials.bricks + kitchenMaterials.bricks+livingRoomMaterials.bricks),
-            cement: parseInt(roomMaterials.cement + bathMaterials.cement + kitchenMaterials.cement+livingRoomMaterials.cement + roofMaterials.cement + floorMaterials.cement),
-            sand: parseInt(roomMaterials.sand + bathMaterials.sand + kitchenMaterials.sand +livingRoomMaterials.sand+ roofMaterials.sand + floorMaterials.sand),
+            bricks: parseInt(roomMaterials.bricks + bathMaterials.bricks + kitchenMaterials.bricks + livingRoomMaterials.bricks),
+            cement: parseInt(roomMaterials.cement + bathMaterials.cement + kitchenMaterials.cement + livingRoomMaterials.cement + roofMaterials.cement + floorMaterials.cement),
+            sand: parseInt(roomMaterials.sand + bathMaterials.sand + kitchenMaterials.sand + livingRoomMaterials.sand + roofMaterials.sand + floorMaterials.sand),
             crush: parseInt(roofMaterials.crush + floorMaterials.crush),
-            iron: parseInt(roofMaterials.iron)
+            iron: parseInt(roofMaterials.iron),
+            paint: roofMaterials.paint + roomMaterials.paint + bathMaterials.paint + kitchenMaterials.paint + livingRoomMaterials.paint,
+            tiles: parseInt(floorMaterials.tiles + bathMaterials.tiles + kitchenMaterials.tiles)
         }
 
         const prices = this.props.allMaterials
         let totalCost = (totalMaterialsUsed.bricks * prices[0].priceTo) + (totalMaterialsUsed.cement * prices[1].priceTo) + (totalMaterialsUsed.iron * prices[2].priceTo) +
-            (totalMaterialsUsed.sand * prices[3].priceTo) + (totalMaterialsUsed.crush * prices[5].priceTo)
-        console.log("total cost and materials",totalCost,totalMaterialsUsed)
+            (totalMaterialsUsed.sand * prices[3].priceTo) + (totalMaterialsUsed.crush * prices[5].priceTo) + parseInt(totalMaterialsUsed.paint * prices[21].priceTo) +
+            (totalMaterialsUsed.tiles * prices[23].priceTo) + (totalGivensqft * prices[7].priceTo)
+        const percent15 = (totalCost / 100) * 15
+        console.log("total cost ", totalCost, totalMaterialsUsed)
+        totalCost += (percent15+otherCosts);
+        
+        console.log("new total cost and materials", totalCost, totalMaterialsUsed)
         this.setState({
-            materials: [["Bricks", totalMaterialsUsed.bricks], ["Cement", totalMaterialsUsed.cement], ["Iron Bar (Sarya) Kg", totalMaterialsUsed.iron],
-            ["Sand ", totalMaterialsUsed.sand], ["Crush ", totalMaterialsUsed.crush]],
+            materials: [["Bricks (pieces)", totalMaterialsUsed.bricks], ["Cement", totalMaterialsUsed.cement], ["Iron Bar (Sarya) Kg", totalMaterialsUsed.iron],
+            ["Sand ", totalMaterialsUsed.sand], ["Crush ", totalMaterialsUsed.crush], ["Paint (Gallons)", totalMaterialsUsed.paint.toFixed(2)], ["Tiles ", totalMaterialsUsed.tiles]],
             estimatedCost: totalCost,
             show: true
 
@@ -189,7 +247,7 @@ class NCCE extends Component {
                                         </Col>
                                         <Col>
                                             <Form.Label>Total Area</Form.Label>
-                                            <Form.Control type="number" id="totalArea" value={(this.state.plotD1 * this.state.plotD2) }
+                                            <Form.Control type="number" id="totalArea" value={(this.state.plotD1 * this.state.plotD2)}
                                                 onChange={this.onchange.bind(this, "totalArea")} />
                                         </Col>
                                         <Col >
@@ -211,7 +269,7 @@ class NCCE extends Component {
                                             <Form.Label>Dimensions (e.g 12x10)</Form.Label>
                                             <Form.Control placeholder="10" type="number" onChange={this.onchange.bind(this, "roomD2")} />
                                         </Col>
-                                        
+
                                     </Form.Row>
                                     <hr style={{ borderBottom: "2px solid white" }} />
                                     <Form.Row>
@@ -227,7 +285,7 @@ class NCCE extends Component {
                                             <Form.Label>Dimensions (e.g 12x10)</Form.Label>
                                             <Form.Control placeholder="10" type="number" onChange={this.onchange.bind(this, "bathD2")} />
                                         </Col>
-                                        
+
                                     </Form.Row>
                                     <hr style={{ borderBottom: "2px solid white" }} />
                                     <Form.Row>
@@ -243,7 +301,7 @@ class NCCE extends Component {
                                             <Form.Label>Dimensions (e.g 12x10)</Form.Label>
                                             <Form.Control placeholder="10" type="number" onChange={this.onchange.bind(this, "kitchenD2")} />
                                         </Col>
-                                        
+
                                     </Form.Row>
                                     <hr style={{ borderBottom: "2px solid white" }} />
                                     <Form.Row>
@@ -259,7 +317,7 @@ class NCCE extends Component {
                                             <Form.Label>Dimensions (e.g 12x10)</Form.Label>
                                             <Form.Control placeholder="10" type="number" onChange={this.onchange.bind(this, "livingD2")} />
                                         </Col>
-                                        
+
                                     </Form.Row>
                                 </Form>
                             </Col>
@@ -300,7 +358,7 @@ class NCCE extends Component {
                                     })}
                                 </tbody>
                             </Table>
-                            <p style={{color:'red'}}>
+                            <p style={{ color: 'red' }}>
                                 note: This cost is an estimate of grey structure of house without including labour cost and prices of high quality material were used so,
                                 if you want a more detailed or complete estimate please login and contact our architects or builders. Thank you
                         </p>
